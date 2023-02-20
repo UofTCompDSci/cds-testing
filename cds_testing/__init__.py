41410b6c62e66b66dd977322cfde0cdcfa60bd22
David Liu <david@cs.toronto.edu>
2023-02-20 11:59:13 -0500

    Improve test error messages

--- cds_testing/__init__.py
+++ cds_testing/__init__.py
@@ -45,7 +45,7 @@ def make_answer_equality_test(hw, soln, expected_vars: dict[str, dict[str, bool]
 
         student_value = getattr(student_hw, var_name)
         soln_value = getattr(soln_nb, var_name)
-        
+
         # If either variable is of numpy generic type, then we can skip the type check
         # and go straight to the equality check. As some of the values in the hw/soln may be correct
         # but the difference might be <int> vs <numpy.int64>.
@@ -60,13 +60,13 @@ def make_answer_equality_test(hw, soln, expected_vars: dict[str, dict[str, bool]
             assert_list_equality(student_value, soln_value, var_name)
         else:
             msg = (
-                f"ISSUE FOUND: The value of your variable {var_name} does not match the expected value:\n"
+                f"ISSUE FOUND: The value of your variable {var_name} does not match the expected value,\n\n"
                 f"\n"
                 f"Yours:     {student_value}"
                 f"\n"
                 f"Expected:     {soln_value}"
                 f"\n\n"
-                f"In case it helps, your variable {var_name} has type {type(student_value)}.\n"
+                f"In case it helps, your variable {var_name} has type {type(student_value).__name__}.\n"
             )
 
             if isinstance(soln_value, float):
@@ -78,50 +78,47 @@ def make_answer_equality_test(hw, soln, expected_vars: dict[str, dict[str, bool]
 def assert_list_equality(student_value, soln_value, var_name: str):
     """
     Utility function to test the equality of lists. If the lists are large, then only
-    display the first value that is incorrect. 
+    display the first value that is incorrect.
     """
-    msg = ( f"\n"
-        f"ISSUE FOUND: Your list variable {var_name} has a length of {len(student_value)} but should have a length of {len(soln_value)}.\n"
-    )    
+    msg = (
+        f"ISSUE FOUND: Your list variable {var_name} has a length of {len(student_value)} "
+        f"but should have a length of {len(soln_value)}.\n"
+    )
     assert len(student_value) == len(soln_value), msg
-    
-    # For smaller size lists, we can display the entire list when showing the difference 
+
+    # For smaller size lists, we can display the entire list when showing the difference
     if len(soln_value) < 25:
         msg = (
-            f"\n"
-            f"ISSUE FOUND: The value of your list variable {var_name}:\n"
-            f"\n"
+            f"ISSUE FOUND: The value of your list variable {var_name} does not match the expected value.\n\n"
+            f"Yours:\n\n"
             f"     {student_value}"
             f"\n\n"
-            f"is not equal to what we expect:\n"
-            f"\n"
+            f"Expected:\n\n"
             f"     {soln_value}\n"
         )
         assert student_value == soln_value, msg
     else:
-        # If the lists are large, then only search for the first error instead of 
-        # displaying the entire list difference 
+        # If the lists are large, then only search for the first error instead of
+        # displaying the entire list difference
         incorrect_index = -1
         for i in range(len(student_value)):
             if student_value[i] != soln_value[i]:
                 incorrect_index = i
                 break
         msg = (
-            f"\n"
-            f"ISSUE FOUND: The value of your list variable {var_name} is not equal to what we expect\n"
-            f"The first incorrect value in your list occurred at index {incorrect_index}.\n"
-            f"At index {incorrect_index}, your variable {var_name} contained the value:\n"
-            f"\n"
+            f"ISSUE FOUND: The value of your list variable {var_name} is not equal to what we expect.\n"
+            f"The first incorrect value in your list occurred at index {incorrect_index}, "
+            f"that is, at {var_name}[{incorrect_index}].\n\n"
+            f"Yours:\n\n"
             f"     {student_value[incorrect_index]}"
             f"\n\n"
-            f"is not equal to what we expect:\n"
-            f"\n"
+            f"Expected:\n\n"
             f"     {soln_value[incorrect_index]}"
             f"\n"
         )
         assert incorrect_index == -1, msg
 
-         
+
 # Example functions (not to be used in final version)
 
 # @pytest.fixture
